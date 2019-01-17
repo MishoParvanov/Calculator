d43c01d5246ac1ab84eda89409b28ab291e30639
Mihail Parvanov <40798779+MishoParvanov@users.noreply.github.com>
2019-01-17 23:07:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listInput = Console.ReadLine().Split(' ').ToList();
+            double result = 0;
+            var a = double.Parse(listInput[0]);
+            var b = double.Parse(listInput[2]);
+
+            switch (listInput[1])
+            { 
+                    case "+":
+                    result = a + b;
+                        break;
+                    case "-":
+                    result = a - b;
+                    break;
+                    case "*":
+                    result = a * b;
+                    break;
+                    case "/":
+                    result = a / b;
+                    break;
+                    default:
+                        break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
